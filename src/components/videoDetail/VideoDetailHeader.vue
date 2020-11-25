503dce402b744b94eb034eff646cac723d03cbd4
h666t <1845720928@qq.com>
2020-11-25 19:42:43 +0800

    建立VideoDetailBody组件

---
+++ src/components/videoDetail/VideoDetailHeader.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="wrapper">
+  <img src="//i.loli.net/2020/11/25/r1yIYuzLxJ3AX7G.png" alt="">
+  <div class="header">
+    <h1>{{detail.title}}</h1>
+    <div class="video">
+      <video
+          ref="video"
+          :poster="detail.cover"
+          controls>
+        <source :src="detail.src"/>
+        您的浏览器不支持视频播放
+      </video>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+props:{
+  detail:{type:Object}
+},
+  updated() {
+    this.$refs.video.load()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/helper.scss";
+
+.wrapper {
+  display: flex;
+  align-items: center;
+  background: $backgroundColor;
+  flex-direction: column;
+  position: relative;
+  >img{
+    width: 320px;
+  }
+  > .header {
+    position: absolute;
+    margin-top: 20px;
+    video {
+      object-fit: fill;
+      max-width: 300px;
+      max-height: 240px;
+      -webkit-border-radius: 14px;
+      -moz-border-radius: 14px;
+      border-radius: 14px;
+    }
+    >h1{
+      font-size: 16px;
+      margin: 10px 0;
+    }
+  }
+}
+</style>
--- src/views/VideoDetail.vue
+++ src/views/VideoDetail.vue
@@ -1,35 +1,28 @@
 <template>
   <div class="wrapper">
-    <div class="header">
-      <h1>{{detail.title}}</h1>
-      <div class="video">
-        <video
-            ref="video"
-            :poster="detail.cover"
-            controls>
-          <source :src="detail.src"/>
-          您的浏览器不支持视频播放
-        </video>
-      </div>
-    </div>
+    <VideoDetailHeader :detail="detail"/>
+    <VideoDetailBody/>
   </div>
 </template>
 
 <script>
+import VideoDetailHeader from "@/components/videoDetail/VideoDetailHeader"
+import  VideoDetailBody from '@/components/videoDetail/VideoDetailBody'
 export default {
+  components: {VideoDetailHeader,VideoDetailBody},
   mounted() {
-    setTimeout(()=>{
+    setTimeout(() => {
       this.detail = {
-        title:'又是一年杜鹃开',
-        cover:'//i.loli.net/2020/11/22/P5AqxEclZpnwaGS.png',
-         src:'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
+        title: '又是一年杜鹃开',
+        cover: '//i.loli.net/2020/11/22/P5AqxEclZpnwaGS.png',
+        src: 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4'
       }
-      this.$refs.video.load()
-    },1000)
+
+    }, 1000)
   },
   data() {
     return {
-      detail:{}
+      detail: {}
     }
   },
 }
@@ -43,17 +36,6 @@ export default {
   align-items: center;
   background: $backgroundColor;
   flex-direction: column;
-  background:  url("//i.loli.net/2020/11/21/Lq5ygQm4a6xFXHb.png");
-
-  > .header {
-    border: 1px solid red;
-
-    video {
-      object-fit: fill;
-      max-width: 300px;
-      max-height: 240px;
-    }
-  }
-
+  background: url("//i.loli.net/2020/11/21/Lq5ygQm4a6xFXHb.png");
 }
 </style>
